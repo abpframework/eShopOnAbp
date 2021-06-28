b369ad1b77eb66b2d6eb66e034799197bb117e57
Galip Tolga Erdem <galiperdem@gmail.com>
2021-06-28 17:43:05 +0300

    Added SaasService Db migrations and db context configurations

---
+++ services/saas/src/EShopOnAbp.SaasService.EntityFrameworkCore/EntityFrameworkCore/SaasServiceDbContextFactory.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace EShopOnAbp.SaasService.EntityFrameworkCore
+{
+    /* This class is needed for EF Core console commands
+     * (like Add-Migration and Update-Database commands)
+     * */
+    public class SaasServiceDbContextFactory : IDesignTimeDbContextFactory<SaasServiceDbContext>
+    {
+        public SaasServiceDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<SaasServiceDbContext>()
+                .UseSqlServer(GetConnectionStringFromConfiguration(), b =>
+                {
+                    b.MigrationsHistoryTable("__SaasService_Migrations");
+                });
+
+            return new SaasServiceDbContext(builder.Options);
+        }
+
+        private static string GetConnectionStringFromConfiguration()
+        {
+            return BuildConfiguration()
+                .GetConnectionString(SaasServiceDbProperties.ConnectionStringName);
+        }
+
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(
+                    Path.Combine(
+                        Directory.GetCurrentDirectory(),
+                        $"..{Path.DirectorySeparatorChar}EShopOnAbp.SaasService.HttpApi.Host"
+                    )
+                )
+                .AddJsonFile("appsettings.json", optional: false);
+
+            return builder.Build();
+        }
+    }
+}
---
+++ services/saas/src/EShopOnAbp.SaasService.EntityFrameworkCore/Migrations/20210628143749_Initial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShopOnAbp.SaasService.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AbpTenants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    DeleterId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpTenants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AbpTenantConnectionStrings",
+                columns: table => new
+                {
+                    TenantId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AbpTenantConnectionStrings", x => new { x.TenantId, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AbpTenantConnectionStrings_AbpTenants_TenantId",
+                        column: x => x.TenantId,
+                        principalTable: "AbpTenants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpTenants_Name",
+                table: "AbpTenants",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AbpTenantConnectionStrings");
+
+            migrationBuilder.DropTable(
+                name: "AbpTenants");
+        }
+    }
+}
