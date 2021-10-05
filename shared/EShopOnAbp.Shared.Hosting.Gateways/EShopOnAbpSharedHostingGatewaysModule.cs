fbcfe6cfef6713f893e2b462be45edd2068084be
Galip Tolga Erdem <galiperdem@gmail.com>
2021-10-05 15:57:56 +0300

    Added consul

--- shared/EShopOnAbp.Shared.Hosting.Gateways/EShopOnAbpSharedHostingGatewaysModule.cs
+++ shared/EShopOnAbp.Shared.Hosting.Gateways/EShopOnAbpSharedHostingGatewaysModule.cs
@@ -1,6 +1,7 @@
 ﻿using EShopOnAbp.Shared.Hosting.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
+using Ocelot.Provider.Consul;
 using Ocelot.Provider.Polly;
 using Volo.Abp.AspNetCore.Mvc.UI.MultiTenancy;
 using Volo.Abp.Modularity;
@@ -20,6 +21,7 @@ namespace EShopOnAbp.Shared.Hosting.Gateways
             var configuration = context.Services.GetConfiguration();
 
             context.Services.AddOcelot(configuration)
+                .AddConsul()
                 .AddPolly();
         }
     }
