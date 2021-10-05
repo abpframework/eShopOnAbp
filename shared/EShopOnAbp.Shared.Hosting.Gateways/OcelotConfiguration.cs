c3aa39d6d32e819dc05fd05dd86db7d84c8058e5
Galip Tolga Erdem <galiperdem@gmail.com>
2021-10-05 15:57:11 +0300

    Added OcelotConfiguration file

---
+++ shared/EShopOnAbp.Shared.Hosting.Gateways/OcelotConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace EShopOnAbp.Shared.Hosting.Gateways
+{
+    public class OcelotConfiguration
+    {
+        public string ServiceName { get; set; }
+        public string DownstreamPathTemplate { get; set; }
+        public string DownstreamScheme { get; set; }
+        public string UpstreamPathTemplate { get; set; }
+        public List<string> UpstreamHttpMethod { get; set; }
+        public List<HostAndPort> DownstreamHostAndPorts { get; set; }
+    }
+
+    public class HostAndPort
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
