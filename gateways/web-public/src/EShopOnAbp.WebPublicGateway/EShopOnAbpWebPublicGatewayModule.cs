55671fb266e70f8b4b24b0caaf52705e6a18f521
Galip Tolga Erdem <galiperdem@gmail.com>
2021-10-06 10:52:37 +0300

    Removed swagger auth from gateways

--- gateways/web-public/src/EShopOnAbp.WebPublicGateway/EShopOnAbpWebPublicGatewayModule.cs
+++ gateways/web-public/src/EShopOnAbp.WebPublicGateway/EShopOnAbpWebPublicGatewayModule.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.Middleware;
 using System.Collections.Generic;
+using System.Linq;
+using EShopOnAbp.Shared.Hosting.AspNetCore;
 using Volo.Abp;
 using Volo.Abp.Account;
 using Volo.Abp.Modularity;
@@ -21,15 +23,8 @@ namespace EShopOnAbp.WebPublicGateway
         {
             var configuration = context.Services.GetConfiguration();
             var hostingEnvironment = context.Services.GetHostingEnvironment();
-
-            SwaggerWithAuthConfigurationHelper.Configure(
-                context: context,
-                authority: configuration["AuthServer:Authority"],
-                scopes: new Dictionary<string, string> /* Requested scopes for authorization code request and descriptions for swagger UI only */
-                    {
-                    },
-                apiTitle: "WebPublic Gateway API"
-            );
+            
+            SwaggerConfigurationHelper.Configure(context, "WebPublic Gateway");
         }
 
         public override void OnApplicationInitialization(ApplicationInitializationContext context)
@@ -46,10 +41,17 @@ namespace EShopOnAbp.WebPublicGateway
             app.UseSwagger();
             app.UseSwaggerUI(options =>
             {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Web Public Gateway API");
                 var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
-                options.OAuthClientId(configuration["AuthServer:SwaggerClientId"]);
-                options.OAuthClientSecret(configuration["AuthServer:SwaggerClientSecret"]);
+                var routes = configuration.GetSection("Routes").Get<List<OcelotConfiguration>>();
+                foreach (var config in routes.GroupBy(t => t.ServiceName).Select(r => r.First()).Distinct())
+                {
+                    var url =
+                        $"{config.DownstreamScheme}://{config.DownstreamHostAndPorts.FirstOrDefault()?.Host}:{config.DownstreamHostAndPorts.FirstOrDefault()?.Port}";
+
+                    options.SwaggerEndpoint($"{url}/swagger/v1/swagger.json", $"{config.ServiceName} API");
+                    options.OAuthClientId(configuration["AuthServer:SwaggerClientId"]);
+                    options.OAuthClientSecret(configuration["AuthServer:SwaggerClientSecret"]);
+                }
             });
             app.UseAbpSerilogEnrichers();
             app.MapWhen(
