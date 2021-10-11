4bbd20ad9d7148e0d75592c64c1ccfdfec88beb9
enisn <info@enisnecipoglu.com>
2021-10-11 16:38:59 +0300

    Add MigrationEventHandler to CatalogService

---
+++ services/catalog/src/EShopOnAbp.CatalogService.HttpApi.Host/DbMigrations/CatalogServiceDatabaseMigrationEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using EShopOnAbp.CatalogService.EntityFrameworkCore;
+using EShopOnAbp.Shared.Hosting.Microservices.DbMigrations;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.EventBus.Distributed;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.TenantManagement;
+using Volo.Abp.Uow;
+
+namespace EShopOnAbp.CatalogService.DbMigrations
+{
+    public class CatalogServiceDatabaseMigrationEventHandler
+        : DatabaseMigrationEventHandlerBase<CatalogServiceDbContext>,
+        IDistributedEventHandler<ApplyDatabaseMigrationsEto>
+    {
+        public CatalogServiceDatabaseMigrationEventHandler(ICurrentTenant currentTenant, IUnitOfWorkManager unitOfWorkManager, ITenantStore tenantStore, ITenantRepository tenantRepository, IDistributedEventBus distributedEventBus, string databaseName) : base(currentTenant, unitOfWorkManager, tenantStore, tenantRepository, distributedEventBus, databaseName)
+        {
+        }
+
+        public async Task HandleEventAsync(ApplyDatabaseMigrationsEto eventData)
+        {
+            if (eventData.DatabaseName != DatabaseName)
+            {
+                return;
+            }
+
+            if (eventData.TenantId != null)
+            {
+                return;
+            }
+
+            try
+            {
+                await MigrateDatabaseSchemaAsync(null);
+            }
+            catch (Exception ex)
+            {
+                await HandleErrorOnApplyDatabaseMigrationAsync(eventData, ex);
+            }
+        }
+    }
+}
