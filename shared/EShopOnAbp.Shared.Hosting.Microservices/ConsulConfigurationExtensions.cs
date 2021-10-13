f9b57aad90d81fa89a4aab395fe6a790d59d2146
Başak ERDEM <galiperdem@gmail.com>
2021-10-13 20:08:33 +0300

    Added consul configuration

---
+++ shared/EShopOnAbp.Shared.Hosting.Microservices/ConsulConfigurationExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EShopOnAbp.Shared.Hosting.Microservices
+{
+    public static class ConsulConfigurationExtensions
+    {
+        public static IServiceCollection AddConsulConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IConsulClient>(p => new ConsulClient(consulConfig =>
+            {
+                var address = configuration.GetValue<string>("Consul:Host");
+                consulConfig.Address = new Uri(address);
+            }));
+            return services;
+        }
+
+        public static string UseConsul(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var configuration = scope.ServiceProvider.GetService<IConfiguration>();
+
+                bool isEnabled = configuration.GetValue<bool>("Consul:Enabled");
+                string serviceName = configuration.GetValue<string>("Consul:Service");
+                string host = configuration.GetValue<string>("Consul:Host");
+                var appString = configuration.GetValue<string>("App:SelfUrl");
+                Uri appUrl = new Uri(appString, UriKind.Absolute);
+
+                if (!isEnabled)
+                    return String.Empty;
+
+                Guid serviceId = Guid.NewGuid();
+                string consulServiceID = $"{serviceName}:{serviceId}";
+
+                var client = scope.ServiceProvider.GetService<IConsulClient>();
+
+                var consulServiceRistration = new AgentServiceRegistration
+                {
+                    Name = serviceName,
+                    ID = consulServiceID,
+                    Address = appUrl.Host,
+                    Port = appUrl.Port
+                };
+
+                client.Agent.ServiceRegister(consulServiceRistration);
+
+                return consulServiceID;
+            }
+        }
+    }
+}
