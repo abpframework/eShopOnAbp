9c86502c06ad6c78fcd9cf7bb2673f8d893eac21
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-12-07 10:51:56 +0300

    Created catalog domain and application layers

--- services/catalog/src/EShopOnAbp.CatalogService.Application.Contracts/Permissions/CatalogServicePermissions.cs
+++ services/catalog/src/EShopOnAbp.CatalogService.Application.Contracts/Permissions/CatalogServicePermissions.cs
@@ -1,10 +1,21 @@
-﻿namespace EShopOnAbp.CatalogService.Permissions
+﻿using Volo.Abp.Reflection;
+
+namespace EShopOnAbp.CatalogService.Permissions
 {
     public static class CatalogServicePermissions
     {
         public const string GroupName = "CatalogService";
 
-        //Add your own permission names. Example:
-        //public const string MyPermission1 = GroupName + ".MyPermission1";
+        public static class Products
+        {
+            public const string Default = GroupName + ".Products";
+            public const string Delete = Default + ".Delete";
+            public const string Update = Default + ".Update";
+            public const string Create = Default + ".Create";
+        }
+        public static string[] GetAll()
+        {
+            return ReflectionHelper.GetPublicConstantsRecursively(typeof(CatalogServicePermissions));
+        }
     }
 }
---
+++ services/catalog/src/EShopOnAbp.CatalogService.Application.Contracts/Products/IPublicProductAppService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace EShopOnAbp.CatalogService.Products
+{
+    public interface IPublicProductAppService : IApplicationService
+    {
+        Task<ListResultDto<ProductDto>> GetListAsync();
+    }
+}
