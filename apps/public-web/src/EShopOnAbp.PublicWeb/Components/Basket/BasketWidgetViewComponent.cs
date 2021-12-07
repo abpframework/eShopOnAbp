a544e889ef62309a3fad442a4f3df461527f1b52
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-12-07 17:09:23 +0300

    Created BasketWidgetViewComponent

---
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Components/Basket/BasketWidgetViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+
+namespace EShopOnAbp.PublicWeb.Components.Basket;
+
+[Widget]
+public class BasketWidgetViewComponent : AbpViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("~/Components/Basket/Default.cshtml");
+    }
+}
