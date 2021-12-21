95f482c76b2228000d339b4c0e1ce45f662f72bd
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-21 15:20:16 +0300

    added cart-widget

--- apps/public-web/src/EShopOnAbp.PublicWeb/Components/Toolbar/Cart/CartWidgetViewComponent.cs
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Components/Toolbar/Cart/CartWidgetViewComponent.cs
@@ -1,29 +1,68 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using EShopOnAbp.BasketService;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+using Volo.Abp.Guids;
+using Volo.Abp.Security.Claims;
+using Volo.Abp.Users;
 
 namespace EShopOnAbp.PublicWeb.Components.Toolbar.Cart;
 
 [Widget(
     AutoInitialize = true,
     RefreshUrl = "/Widgets/Cart",
-    StyleFiles = new[] {"/components/cart/cart-widget.css"},
+    StyleTypes = new[] {typeof(CartWidgetStyleContributor)},
     ScriptTypes = new[] {typeof(CartWidgetScriptContributor)}
 )]
 public class CartWidgetViewComponent : AbpViewComponent
 {
     private readonly IBasketAppService _basketAppService;
+    private readonly IGuidGenerator _guidGenerator;
+    private readonly ICurrentUser _currentUser;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public CartWidgetViewComponent(IBasketAppService basketAppService)
+    protected HttpContext HttpContext => _httpContextAccessor.HttpContext;
+    protected const string AnonymousUserCookieName = "eshop_anonymousId";
+
+    public CartWidgetViewComponent(
+        IBasketAppService basketAppService,
+        ICurrentUser currentUser,
+        IGuidGenerator guidGenerator,
+        IHttpContextAccessor httpContextAccessor)
     {
         _basketAppService = basketAppService;
+        _currentUser = currentUser;
+        _httpContextAccessor = httpContextAccessor;
+        _guidGenerator = guidGenerator;
     }
 
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        var basketDto = await _basketAppService.GetAsync();
+        BasketDto basketDto = new BasketDto();
+        try
+        {
+            if (!_currentUser.IsAuthenticated)
+            {
+                var anonymousUserId = _guidGenerator.Create();
+                HttpContext.Response.Cookies.Append(AnonymousUserCookieName, anonymousUserId.ToString());
+                basketDto = await _basketAppService.GetByAnonymousUserIdAsync(anonymousUserId);
+            }
+            else
+            {
+                //TODO: Merge with anonymously stored cart if exist 
+                basketDto = await _basketAppService.GetAsync();
+            }
+        }
+        catch (Exception e)
+        {
+            basketDto = null;
+            Console.WriteLine(e);
+        }
+
         return View("~/Components/Toolbar/Cart/Default.cshtml", basketDto);
     }
 }
--- apps/public-web/src/EShopOnAbp.PublicWeb/Pages/index.js
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Pages/index.js
@@ -1,13 +1,19 @@
-﻿(function(){
-    $(function(){
+﻿(function () {
+    //set anonymous user
+    let anonymousId = abp.utils.getCookieValue("eshop_anonymousId");
+    if (anonymousId != null) {
+        console.info("Anonymous user set:" + anonymousId);
+    }
+
+    $(function () {
         var basketWidget = new abp.WidgetManager('#BasketArea');
 
-        $('.product-list-item').click(function(){
+        $('.product-list-item').click(function () {
             var $this = $(this);
             var productId = $this.attr('data-product-id');
             eShopOnAbp.basketService.basket.addProduct({
                 productId: productId
-            }).then(function(){
+            }).then(function () {
                 basketWidget.refresh();
                 abp.notify.success("Added product to your basket.", "Successfully added");
             });
