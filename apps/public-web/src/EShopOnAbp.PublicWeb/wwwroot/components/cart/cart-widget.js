daf81bfa28136603a49606cffe7971c432b800eb
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-21 16:15:32 +0300

    updated cart widget

--- apps/public-web/src/EShopOnAbp.PublicWeb/wwwroot/components/cart/cart-widget.js
+++ apps/public-web/src/EShopOnAbp.PublicWeb/wwwroot/components/cart/cart-widget.js
@@ -1,28 +1,15 @@
 (function () {
-    alert("working")
     var connection = new signalR.HubConnectionBuilder()
         .withUrl("/signalr-hubs/basket")
         .build();
 
     connection.on("BasketProductUpdated", function (data) {
-        debugger;
         var widgetManager = $wrapper.data('abp-widget-manager');
+        //todo:test
+        debugger;
         $('#data-cart-count-id').text(data);
         widgetManager.refresh();
         abp.notify.info('The product "' + data.productName + '" has been changed!', 'Your basket has been updated!');
-
-        // $('.basket-list')
-        //     .closest('.abp-widget-wrapper')
-        //     .each(function(){
-        //         var $wrapper = $(this);
-        //         if ($wrapper.find('[data-product-id=' + data.productId + ']').length){
-        //             var widgetManager = new abp.WidgetManager({
-        //                 wrapper: $wrapper
-        //             });
-        //             widgetManager.refresh();
-        //             abp.notify.info('The product "' + data.productName + '" has been changed!', 'Your basket has been updated!');
-        //         }
-        //     });
     });
 
     connection.start().then(function () {
