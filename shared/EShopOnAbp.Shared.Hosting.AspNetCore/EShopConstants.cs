fec82532e297126ebd35a68c7baaf5db5f6e1763
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-23 13:47:10 +0300

    removed cookie constant

--- shared/EShopOnAbp.Shared.Hosting.AspNetCore/EShopConstants.cs
+++ shared/EShopOnAbp.Shared.Hosting.AspNetCore/EShopConstants.cs
@@ -2,6 +2,6 @@
 
 public static class EShopConstants
 {
-    public const string AnonymousUserCookieName = "eshop_anonymousId";
+    // public const string AnonymousUserCookieName = "eshop_anonymousId";
     public const string AnonymousUserClaimName = "anonymous_id";
 }
