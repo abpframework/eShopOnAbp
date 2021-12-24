9fa780d4f15ef17920b4e92bfbe9600fa5db3bd7
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-24 14:22:34 +0300

    added payment widget

---
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Components/Payment/PaymentWidgetStyleContributor.cs
@@ -0,0 +1,7 @@
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+
+namespace EShopOnAbp.PublicWeb.Components.Payment;
+
+public class PaymentWidgetStyleContributor: BundleContributor
+{
+}
---
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Components/Payment/PaymentWidgetViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Widgets;
+
+namespace EShopOnAbp.PublicWeb.Components.Payment;
+
+[Widget(
+    AutoInitialize = true,
+    RefreshUrl = "/Widgets/Payment",
+    StyleTypes = new[] {typeof(PaymentWidgetStyleContributor)},
+    ScriptTypes = new[] {typeof(PaymentWidgetScriptContributor)}
+)]
+public class PaymentWidgetViewComponent: AbpViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult(View("~/Components/Payment/Default.cshtml"));
+    }
+}
