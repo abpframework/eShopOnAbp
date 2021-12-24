6982675c64532c9affaaea942af83f033a130242
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-24 16:13:02 +0300

    added payment completed page

---
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Pages/PaymentCompleted.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace EShopOnAbp.PublicWeb.Pages;
+
+[Authorize]
+public class PaymentCompletedModel : AbpPageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
