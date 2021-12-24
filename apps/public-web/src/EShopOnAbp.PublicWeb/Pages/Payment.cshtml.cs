11b794b0cef18f72259f34bae987c7a5583bc9df
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-24 12:23:00 +0300

    added payment page

---
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Pages/Payment.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace EShopOnAbp.PublicWeb.Pages;
+
+public class PaymentModel : AbpPageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
