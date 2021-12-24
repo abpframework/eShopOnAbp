91f9b159eee3b267c531a650dd4fd54f0412e57b
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-24 16:13:19 +0300

    updated payment page

--- apps/public-web/src/EShopOnAbp.PublicWeb/Pages/Payment.cshtml.cs
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Pages/Payment.cshtml.cs
@@ -1,10 +1,30 @@
-﻿using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+﻿using System.Threading.Tasks;
+using EShopOnAbp.BasketService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
 
 namespace EShopOnAbp.PublicWeb.Pages;
 
+[Authorize]
 public class PaymentModel : AbpPageModel
 {
+    private readonly IBasketAppService _basketAppService;
+
+    public PaymentModel(IBasketAppService basketAppService)
+    {
+        _basketAppService = basketAppService;
+    }
+
     public void OnGet()
     {
+    }
+    public async Task<IActionResult> OnPostAsync()
+    {
+        Logger.LogInformation("Payment Proceeded...");
+        await _basketAppService.PurchaseAsync();
+
+        return RedirectToPage("PaymentCompleted");
     }
 }
