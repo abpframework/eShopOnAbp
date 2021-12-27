aef9320bb93464c1ffa8233beb14096914918c6f
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-27 22:21:16 +0300

    updated payment widget

--- apps/public-web/src/EShopOnAbp.PublicWeb/Components/Payment/PaymentWidgetViewComponent.cs
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Components/Payment/PaymentWidgetViewComponent.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EShopOnAbp.BasketService;
 using EShopOnAbp.PublicWeb.Basket;
 using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
@@ -12,17 +14,30 @@ namespace EShopOnAbp.PublicWeb.Components.Payment;
     StyleTypes = new[] {typeof(PaymentWidgetStyleContributor)},
     ScriptTypes = new[] {typeof(PaymentWidgetScriptContributor)}
 )]
-public class PaymentWidgetViewComponent: AbpViewComponent
+public class PaymentWidgetViewComponent : AbpViewComponent
 {
     private readonly UserBasketProvider _userBasketProvider;
+    private readonly UserAddressProvider _userAddressProvider;
 
-    public PaymentWidgetViewComponent(UserBasketProvider userBasketProvider)
+    public PaymentWidgetViewComponent(UserBasketProvider userBasketProvider, UserAddressProvider userAddressProvider)
     {
         _userBasketProvider = userBasketProvider;
+        _userAddressProvider = userAddressProvider;
     }
 
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        return View("~/Components/Payment/Default.cshtml", await _userBasketProvider.GetBasketAsync());
+        var viewModel = new PaymentViewModel()
+        {
+            Basket = await _userBasketProvider.GetBasketAsync(),
+            Address = _userAddressProvider.GetDemoAddresses()
+        };
+        return View("~/Components/Payment/Default.cshtml", viewModel);
     }
+}
+
+public class PaymentViewModel
+{
+    public BasketDto Basket { get; set; }
+    public List<AddressDto> Address { get; set; }
 }
