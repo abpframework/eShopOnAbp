d58cf4be663d769376e8e398a6e09e5a91ffd9c1
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-27 22:21:09 +0300

    added user-address provider

---
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Basket/UserAddressProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Volo.Abp.DependencyInjection;
+
+namespace EShopOnAbp.PublicWeb.Basket;
+
+public class UserAddressProvider : ITransientDependency
+{
+    public List<AddressDto> GetDemoAddresses()
+    {
+        return new List<AddressDto>()
+        {
+            new()
+            {
+                Name = "Home",
+                Description = "Cecilia Chapman Senior 711-2880 Nulla St. Mankato Mississippi 96522/USA",
+                IsDefault = true
+            },
+            new()
+            {
+                Name = "Work",
+                Description = "Yeşilköy Serbest Bölge Mah. E-Blok Sokak E1 Blok No:2, Bakırköy/İstanbul"
+            }
+        };
+    }
+}
+
+public class AddressDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool IsDefault { get; set; } = false;
+}
