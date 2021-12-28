55699a144bacfa9cf9f338e44f3ec22d28eb77b0
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-28 11:13:12 +0300

    updated addressProvider and address selection

--- apps/public-web/src/EShopOnAbp.PublicWeb/EShopOnAbpPaymentConsts.cs
+++ apps/public-web/src/EShopOnAbp.PublicWeb/EShopOnAbpPaymentConsts.cs
@@ -3,5 +3,11 @@
     public static class EShopOnAbpPaymentConsts
     {
         public const string Currency = "USD";
+
+        public static class DemoAddressTypes
+        {
+            public const string Work = "Work";
+            public const string Home = "Home";
+        }
     }
 }
