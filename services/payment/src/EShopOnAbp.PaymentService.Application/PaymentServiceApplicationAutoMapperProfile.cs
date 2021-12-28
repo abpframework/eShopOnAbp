4c74f20347326709dfef670bb31b18d9e146d366
enisn <info@enisnecipoglu.com>
2021-12-28 10:46:19 +0300

    Add missing mappings

--- services/payment/src/EShopOnAbp.PaymentService.Application/PaymentServiceApplicationAutoMapperProfile.cs
+++ services/payment/src/EShopOnAbp.PaymentService.Application/PaymentServiceApplicationAutoMapperProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using EShopOnAbp.PaymentService.PaymentRequests;
+using Volo.Abp.AutoMapper;
 
 namespace EShopOnAbp.PaymentService
 {
@@ -6,6 +8,9 @@ namespace EShopOnAbp.PaymentService
     {
         public PaymentServiceApplicationAutoMapperProfile()
         {
+            CreateMap<PaymentRequest, PaymentRequestDto>();
+            CreateMap<PaymentRequestProduct, PaymentRequestProductDto>();
+
             /* You can configure your AutoMapper mapping configuration here.
              * Alternatively, you can split your mapping configurations
              * into multiple profile classes for a better organization. */
