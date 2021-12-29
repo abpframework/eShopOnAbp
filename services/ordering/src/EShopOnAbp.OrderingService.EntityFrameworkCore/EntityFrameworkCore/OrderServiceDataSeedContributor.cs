17365b364435bfec6dbf712247c4cedd2b66ac65
Galip Tolga Erdem <galiperdem@gmail.com>
2021-12-29 14:36:45 +0300

    added order seed contributor comments

--- services/ordering/src/EShopOnAbp.OrderingService.EntityFrameworkCore/EntityFrameworkCore/OrderServiceDataSeedContributor.cs
+++ services/ordering/src/EShopOnAbp.OrderingService.EntityFrameworkCore/EntityFrameworkCore/OrderServiceDataSeedContributor.cs
@@ -8,6 +8,10 @@ using Volo.Abp.DependencyInjection;
 
 namespace EShopOnAbp.OrderingService.EntityFrameworkCore;
 
+/// <summary>
+/// DataSeedContributor for seeding pre-exist order status data.
+/// This is a sample for seeding data without DbSet (non-aggregate data), without using repository.
+/// </summary>
 public class OrderServiceDataSeedContributor : IDataSeedContributor, ITransientDependency
 {
     private readonly IOrderingServiceDbContext _dbContext;
@@ -26,15 +30,14 @@ public class OrderServiceDataSeedContributor : IDataSeedContributor, ITransientD
     {
         if (!await _dbContext.Set<OrderStatus>().AnyAsync())
         {
-            var orderStatusList = GetOrderStatusList();
-            _dbContext.Set<OrderStatus>().AddRange(orderStatusList);
+            _dbContext.Set<OrderStatus>().AddRange(GetOrderStatusList());
             await _dbContext.SaveChangesAsync();
         }
     }
 
     private List<OrderStatus> GetOrderStatusList()
     {
-        return new List<OrderStatus>()
+        return new List<OrderStatus>
         {
             OrderStatus.Submitted,
             OrderStatus.AwaitingValidation,
