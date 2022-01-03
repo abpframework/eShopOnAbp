46809d2c073142231f835c0604ee8dc218612650
Galip Tolga Erdem <galiperdem@gmail.com>
2022-01-03 22:13:37 +0300

    updated basket service to handle orderplaced eto

--- services/basket/src/EShopOnAbp.BasketService.Domain/Orders/OrderPlacedEventHandler.cs
+++ services/basket/src/EShopOnAbp.BasketService.Domain/Orders/OrderPlacedEventHandler.cs
@@ -5,20 +5,18 @@ using Volo.Abp.EventBus.Distributed;
 
 namespace EShopOnAbp.BasketService.Orders
 {
-    public class OrderAcceptedEventHandler :
-        IDistributedEventHandler<OrderAcceptedEto>,
-        ITransientDependency
+    public class OrderPlacedEventHandler : IDistributedEventHandler<OrderPlacedEto>, ITransientDependency
     {
         private readonly IBasketRepository _basketRepository;
 
-        public OrderAcceptedEventHandler(IBasketRepository basketRepository)
+        public OrderPlacedEventHandler(IBasketRepository basketRepository)
         {
             _basketRepository = basketRepository;
         }
 
-        public async Task HandleEventAsync(OrderAcceptedEto eventData)
+        public async Task HandleEventAsync(OrderPlacedEto eventData)
         {
-            var basket = await _basketRepository.GetAsync(eventData.BuyerId);
+            var basket = await _basketRepository.GetAsync(eventData.Buyer.BuyerId);
             basket.Clear();
             await _basketRepository.UpdateAsync(basket);
         }
