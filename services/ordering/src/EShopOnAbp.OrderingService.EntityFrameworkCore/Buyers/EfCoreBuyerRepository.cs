fde19634df17ba23a6b5a5fdfc9005df878074ea
Galip Tolga Erdem <galiperdem@gmail.com>
2022-01-04 12:41:23 +0300

    Added OrderManager unit test and repository updates with eager loading

--- services/ordering/src/EShopOnAbp.OrderingService.EntityFrameworkCore/Buyers/EfCoreBuyerRepository.cs
+++ services/ordering/src/EShopOnAbp.OrderingService.EntityFrameworkCore/Buyers/EfCoreBuyerRepository.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using EShopOnAbp.OrderingService.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -10,5 +14,19 @@ public class EfCoreBuyerRepository : EfCoreRepository<OrderingServiceDbContext, 
     public EfCoreBuyerRepository(IDbContextProvider<OrderingServiceDbContext> dbContextProvider) : base(
         dbContextProvider)
     {
+    }
+
+    public override async Task<Buyer> InsertAsync(Buyer entity, bool autoSave = false,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        var newEntity = await base.InsertAsync(entity, autoSave, cancellationToken);
+        await EnsurePropertyLoadedAsync(newEntity, query => query.PaymentType, cancellationToken);
+        return newEntity;
+    }
+
+    public override async Task<IQueryable<Buyer>> WithDetailsAsync()
+    {
+        return (await GetQueryableAsync())
+            .Include(q => q.PaymentType);
     }
 }
--- services/ordering/src/EShopOnAbp.OrderingService.EntityFrameworkCore/Orders/EfCoreOrderRepository.cs
+++ services/ordering/src/EShopOnAbp.OrderingService.EntityFrameworkCore/Orders/EfCoreOrderRepository.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using EShopOnAbp.OrderingService.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -7,7 +11,22 @@ namespace EShopOnAbp.OrderingService.Orders;
 
 public class EfCoreOrderRepository : EfCoreRepository<OrderingServiceDbContext, Order, Guid>, IOrderRepository
 {
-    public EfCoreOrderRepository(IDbContextProvider<OrderingServiceDbContext> dbContextProvider) : base(dbContextProvider)
+    public EfCoreOrderRepository(IDbContextProvider<OrderingServiceDbContext> dbContextProvider) : base(
+        dbContextProvider)
     {
+    }
+
+    public override async Task<Order> InsertAsync(Order entity, bool autoSave = false,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        var newEntity = await base.InsertAsync(entity, autoSave, cancellationToken);
+        await EnsurePropertyLoadedAsync(newEntity, o => o.OrderStatus, cancellationToken);
+        return newEntity;
+    }
+
+    public override async Task<IQueryable<Order>> WithDetailsAsync()
+    {
+        return (await GetQueryableAsync())
+            .Include(q => q.OrderStatus);
     }
 }
