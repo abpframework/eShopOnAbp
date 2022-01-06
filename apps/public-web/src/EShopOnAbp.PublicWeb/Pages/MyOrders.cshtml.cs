8c62e6e1458ea74c0e200a304613b3b1f4e1da6b
Galip Tolga Erdem <galiperdem@gmail.com>
2022-01-06 17:21:37 +0300

    added initial myOrder page

---
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Pages/MyOrders.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EShopOnAbp.OrderingService.Orders;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace EShopOnAbp.PublicWeb.Pages;
+
+public class MyOrdersModel : AbpPageModel
+{
+    private readonly IOrderAppService _orderAppService;
+    public List<OrderDto> MyOrders { get; set; }
+
+    public MyOrdersModel(IOrderAppService orderAppService)
+    {
+        _orderAppService = orderAppService;
+    }
+
+    public async Task OnGet()
+    {
+        MyOrders = await _orderAppService.GetMyOrdersAsync(new GetMyOrdersInput());
+    }
+}
