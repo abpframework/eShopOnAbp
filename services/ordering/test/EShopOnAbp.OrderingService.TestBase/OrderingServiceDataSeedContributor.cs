544188e185f69881c36f92fe029ab7a109ebc61f
Galip Tolga Erdem <galiperdem@gmail.com>
2022-01-06 14:37:22 +0300

    added uow

--- services/ordering/test/EShopOnAbp.OrderingService.TestBase/OrderingServiceDataSeedContributor.cs
+++ services/ordering/test/EShopOnAbp.OrderingService.TestBase/OrderingServiceDataSeedContributor.cs
@@ -5,6 +5,7 @@ using EShopOnAbp.OrderingService.Orders;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Guids;
+using Volo.Abp.Uow;
 
 namespace EShopOnAbp.OrderingService
 {
@@ -13,15 +14,17 @@ namespace EShopOnAbp.OrderingService
         private readonly OrderManager _orderManager;
         private readonly TestData _testData;
         private readonly TestProducts _testProducts;
+        private readonly UnitOfWorkManager _unitOfWorkManager;
 
         public OrderingServiceDataSeedContributor(
             OrderManager orderManager,
             TestData testData,
-            TestProducts testProducts)
+            TestProducts testProducts, UnitOfWorkManager unitOfWorkManager)
         {
             _orderManager = orderManager;
             _testData = testData;
             _testProducts = testProducts;
+            _unitOfWorkManager = unitOfWorkManager;
         }
 
         public async Task SeedAsync(DataSeedContext context)
@@ -31,14 +34,22 @@ namespace EShopOnAbp.OrderingService
 
         private async Task SeedTestOrdersAsync()
         {
-            var order1 = await _orderManager.CreateOrderAsync(
-                1, _testData.CurrentUserId, _testData.CurrentUserName, _testData.CurrentUserEmail,
-                _testProducts.GetRandomProducts(0),
-                _testData.Address.Street,
-                _testData.Address.City,
-                _testData.Address.Country,
-                _testData.Address.ZipCode
-            );
+            var randomOrderItems = _testProducts.GetRandomProducts(5);
+
+            using (_unitOfWorkManager.Begin())
+            {
+                var order1 = await _orderManager.CreateOrderAsync(
+                    1,
+                    _testData.CurrentUserId,
+                    _testData.CurrentUserName,
+                    _testData.CurrentUserEmail,
+                    randomOrderItems,
+                    _testData.Address.Street,
+                    _testData.Address.City,
+                    _testData.Address.Country,
+                    _testData.Address.ZipCode
+                );
+            }
 
             var order2 = await _orderManager.CreateOrderAsync(
                 1, _testData.CurrentUserId, _testData.CurrentUserName, _testData.CurrentUserEmail,
