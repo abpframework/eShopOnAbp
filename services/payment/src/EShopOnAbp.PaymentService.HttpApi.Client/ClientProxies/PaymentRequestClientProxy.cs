7d2c2c6469038f12a63d85c5f3d04b993406c523
enisn <iletisim@enisnecipoglu.com>
2022-01-17 15:49:03 +0300

    Generate client-proxies for payment service

--- services/payment/src/EShopOnAbp.PaymentService.HttpApi.Client/ClientProxies/PaymentRequestClientProxy.cs
+++ services/payment/src/EShopOnAbp.PaymentService.HttpApi.Client/ClientProxies/PaymentRequestClientProxy.cs
@@ -1,8 +1,7 @@
 // This file is part of PaymentRequestClientProxy, you can customize it here
 // ReSharper disable once CheckNamespace
-namespace EShopOnAbp.PaymentService.Controllers.ClientProxies
+namespace EShopOnAbp.PaymentService.Controllers.ClientProxies;
+
+public partial class PaymentRequestClientProxy
 {
-    public partial class PaymentRequestClientProxy
-    {
-    }
 }
