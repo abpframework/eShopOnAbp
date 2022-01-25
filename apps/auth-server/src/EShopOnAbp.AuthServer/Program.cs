d062d68229b7ea8d2dee0a7c765d584394bfd694
Galip Tolga Erdem <galiperdem@gmail.com>
2022-01-25 22:29:52 +0300

    updated applications

--- apps/auth-server/src/EShopOnAbp.AuthServer/Program.cs
+++ apps/auth-server/src/EShopOnAbp.AuthServer/Program.cs
@@ -1,46 +1,45 @@
 using System;
 using System.Threading.Tasks;
 using EShopOnAbp.Shared.Hosting.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
-namespace EShopOnAbp.AuthServer
+namespace EShopOnAbp.AuthServer;
+
+public class Program
 {
-    public class Program
+    public static async Task<int> Main(string[] args)
     {
-        public static async Task<int> Main(string[] args)
+        var assemblyName = typeof(Program).Assembly.GetName().Name;
+
+        SerilogConfigurationHelper.Configure(assemblyName);
+
+        try
         {
-            var assemblyName = typeof(Program).Assembly.GetName().Name;
-
-            SerilogConfigurationHelper.Configure(assemblyName);
-
-            try
-            {
-                Log.Information($"Starting {assemblyName}.");
-                await CreateHostBuilder(args).Build().RunAsync();
-                return 0;
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, $"{assemblyName} terminated unexpectedly!");
-                return 1;
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-
-        internal static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(build =>
-                {
-                    build.AddJsonFile("appsettings.secrets.json", optional: true);
-                })
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+            Log.Information($"Starting {assemblyName}.");
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host
+                .AddAppSettingsSecretsJson()
                 .UseAutofac()
                 .UseSerilog();
+
+            await builder.AddApplicationAsync<EShopOnAbpAuthServerModule>();
+            var app = builder.Build();
+            await app.InitializeApplicationAsync();
+            await app.RunAsync();
+
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, $"{assemblyName} terminated unexpectedly!");
+            return 1;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
