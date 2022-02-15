17c7830655099ffdda1bf043c1e4c43127322c3d
Galip Tolga Erdem <galiperdem@gmail.com>
2022-02-15 15:12:30 +0300

    added gateway host builder extensions

---
+++ shared/EShopOnAbp.Shared.Hosting.Gateways/GatewayHostBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.Hosting;
+
+public static class AbpHostingHostBuilderExtensions
+{
+    public const string AppOcelotJsonPath = "ocelot.json";
+
+    public static IHostBuilder AddOcelotJson(
+        this IHostBuilder hostBuilder,
+        bool optional = true,
+        bool reloadOnChange = true,
+        string path = AppOcelotJsonPath)
+    {
+        return hostBuilder.ConfigureAppConfiguration((_, builder) =>
+        {
+            builder.AddJsonFile(
+                path: AppOcelotJsonPath,
+                optional: optional,
+                reloadOnChange: reloadOnChange
+            );
+        });
+    }
+}
