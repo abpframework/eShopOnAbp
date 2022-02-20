9c87cf323e3aab218f43b73a96b6a5205a5cf3bb
Galip Tolga Erdem <galiperdem@gmail.com>
2022-02-20 23:37:40 +0300

    Added new migrations about feature management removal

--- services/administration/src/EShopOnAbp.AdministrationService.EntityFrameworkCore/Migrations/20220220203632_Removed-FeatureManagement.Designer.cs
+++ services/administration/src/EShopOnAbp.AdministrationService.EntityFrameworkCore/Migrations/20220220203632_Removed-FeatureManagement.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EShopOnAbp.AdministrationService.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace EShopOnAbp.AdministrationService.Migrations
 {
     [DbContext(typeof(AdministrationServiceDbContext))]
-    partial class AdministrationServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220220203632_Removed-FeatureManagement")]
+    partial class RemovedFeatureManagement
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
