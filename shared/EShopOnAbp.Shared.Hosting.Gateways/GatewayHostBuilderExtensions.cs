e4f144af09faf84be6669e1214921ed61f8533b9
Galip Tolga Erdem <galiperdem@gmail.com>
2022-03-23 21:38:04 +0300

    Added yarp configuration and builder extensions

--- shared/EShopOnAbp.Shared.Hosting.Gateways/GatewayHostBuilderExtensions.cs
+++ shared/EShopOnAbp.Shared.Hosting.Gateways/GatewayHostBuilderExtensions.cs
@@ -4,18 +4,18 @@ namespace Microsoft.Extensions.Hosting;
 
 public static class AbpHostingHostBuilderExtensions
 {
-    public const string AppOcelotJsonPath = "ocelot.json";
+    public const string AppYarpJsonPath = "yarp.json";
 
     public static IHostBuilder AddOcelotJson(
         this IHostBuilder hostBuilder,
         bool optional = true,
         bool reloadOnChange = true,
-        string path = AppOcelotJsonPath)
+        string path = AppYarpJsonPath)
     {
         return hostBuilder.ConfigureAppConfiguration((_, builder) =>
         {
             builder.AddJsonFile(
-                path: AppOcelotJsonPath,
+                path: AppYarpJsonPath,
                 optional: optional,
                 reloadOnChange: reloadOnChange
             );
