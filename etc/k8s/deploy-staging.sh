72cc76827307849bb9955b17a1a9123b242cc3dc
selmankoc <selman.koc@volosoft.com>
2022-06-10 14:56:11 +0300

    changed ingress tls secret

--- etc/k8s/deploy-staging.sh
+++ etc/k8s/deploy-staging.sh
@@ -1,1 +1,1 @@
-helm upgrade --install eshop-st eshoponabp --namespace eshop --create-namespace
+helm upgrade --install eshop-st eshoponabp -f ./eshoponabp/values.yaml --namespace eshop --create-namespace
