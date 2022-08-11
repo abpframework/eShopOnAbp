716aa0460f467cbc5c8abaf05eb8e16282c146e8
selmankoc <selman.koc@volosoft.com>
2022-08-11 13:00:13 +0300

    create push ghcr sh

---
+++ etc/k8s/azure/scripts/push-images-ghcr.sh
@@ -0,0 +1,36 @@
+export version="1.0.0"
+
+az acr login --name volocr
+
+docker tag eshoponabp/app-web:"${version}" ghcr.io/volosoft/eshoponabp/app-web:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/app-web:"${version}"
+
+docker tag eshoponabp/app-authserver:"${version}" ghcr.io/volosoft/eshoponabp/app-authserver:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/app-authserver:"${version}"
+
+docker tag eshoponabp/app-publicweb:"${version}" ghcr.io/volosoft/eshoponabp/app-publicweb:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/app-publicweb:"${version}"
+
+docker tag eshoponabp/gateway-web:"${version}" ghcr.io/volosoft/eshoponabp/gateway-web:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/gateway-web:"${version}"
+
+docker tag eshoponabp/gateway-web-public:"${version}" ghcr.io/volosoft/eshoponabp/gateway-web-public:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/gateway-web-public:"${version}"
+
+docker tag eshoponabp/service-identity:"${version}" ghcr.io/volosoft/eshoponabp/service-identity:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/service-identity:"${version}"
+
+docker tag eshoponabp/service-administration:"${version}" ghcr.io/volosoft/eshoponabp/service-administration:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/service-administration:"${version}"
+
+docker tag eshoponabp/service-basket:"${version}" ghcr.io/volosoft/eshoponabp/service-basket:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/service-basket:"${version}"
+
+docker tag eshoponabp/service-catalog:"${version}" ghcr.io/volosoft/eshoponabp/service-catalog:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/service-catalog:"${version}"
+
+docker tag eshoponabp/service-ordering:"${version}" ghcr.io/volosoft/eshoponabp/service-ordering:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/service-ordering:"${version}"
+
+docker tag eshoponabp/service-payment:"${version}" ghcr.io/volosoft/eshoponabp/service-payment:"${version}"
+docker push ghcr.io/volosoft/eshoponabp/service-payment:"${version}"
