66797de5df80a4f70344c692b6c8ea41dcfd43de
malik masis <malik.masis@gmail.com>
2022-09-21 12:14:25 +0300

    Added the `IsPurschased` condition

--- apps/public-web/src/EShopOnAbp.PublicWeb/Pages/ProductDetail.cshtml.cs
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Pages/ProductDetail.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using EShopOnAbp.CatalogService.Products;
+using EShopOnAbp.OrderingService.Orders;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
 
@@ -12,16 +14,22 @@ namespace EShopOnAbp.PublicWeb.Pages
         public int OrderNo { get; set; }
 
         public ProductDto Product { get; private set; }
+        public bool IsPurschased { get; private set; }
 
         private readonly IPublicProductAppService _productAppService;
+        private readonly IOrderAppService _orderAppService;
 
-        public ProductDetailModel(IPublicProductAppService productAppService)
+        public ProductDetailModel(
+            IPublicProductAppService productAppService,
+            IOrderAppService orderAppService)
         {
             _productAppService = productAppService;
+            _orderAppService = orderAppService;
         }
 
         public async Task OnGet(Guid id)
         {
+            IsPurschased = (await _orderAppService.GetMyOrdersAsync(new GetMyOrdersInput())).Any(p => p.Items.Any(p => p.ProductId == id));
             Product = await _productAppService.GetAsync(id);
         }
     }
