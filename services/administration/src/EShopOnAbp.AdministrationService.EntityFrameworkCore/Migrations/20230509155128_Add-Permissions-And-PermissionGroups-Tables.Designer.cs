661b17f2d763bd64114700a625b2a7332878bf57
jflaga <flaga.jeremiah@gmail.com>
2023-05-09 23:56:17 +0800

    fix for error: relation "AbpPermissionGroups" does not exist

--- services/administration/src/EShopOnAbp.AdministrationService.EntityFrameworkCore/Migrations/20230509155128_Add-Permissions-And-PermissionGroups-Tables.Designer.cs
+++ services/administration/src/EShopOnAbp.AdministrationService.EntityFrameworkCore/Migrations/20230509155128_Add-Permissions-And-PermissionGroups-Tables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EShopOnAbp.AdministrationService.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace EShopOnAbp.AdministrationService.Migrations
 {
     [DbContext(typeof(AdministrationServiceDbContext))]
-    partial class AdministrationServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230509155128_Add-Permissions-And-PermissionGroups-Tables")]
+    partial class AddPermissionsAndPermissionGroupsTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
