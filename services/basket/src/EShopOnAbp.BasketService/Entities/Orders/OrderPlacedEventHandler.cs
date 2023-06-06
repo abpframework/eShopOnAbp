073fecb7e946790b7702ffd375798e930856b2b6
Galip Tolga Erdem <galiperdem@gmail.com>
2023-06-06 16:00:57 -0400

    BasketService updated to use file-scoped namespaces

--- services/basket/src/EShopOnAbp.BasketService/Entities/Orders/OrderPlacedEventHandler.cs
+++ services/basket/src/EShopOnAbp.BasketService/Entities/Orders/OrderPlacedEventHandler.cs
@@ -3,23 +3,22 @@ using Volo.Abp.DependencyInjection;
 using Volo.Abp.EventBus.Distributed;
 using Volo.Abp.Uow;
 
-namespace EShopOnAbp.BasketService.Entities.Orders
+namespace EShopOnAbp.BasketService.Entities.Orders;
+
+public class OrderPlacedEventHandler : IDistributedEventHandler<OrderPlacedEto>, ITransientDependency
 {
-    public class OrderPlacedEventHandler : IDistributedEventHandler<OrderPlacedEto>, ITransientDependency
+    private readonly IBasketRepository _basketRepository;
+
+    public OrderPlacedEventHandler(IBasketRepository basketRepository)
     {
-        private readonly IBasketRepository _basketRepository;
+        _basketRepository = basketRepository;
+    }
 
-        public OrderPlacedEventHandler(IBasketRepository basketRepository)
-        {
-            _basketRepository = basketRepository;
-        }
-
-        [UnitOfWork]
-        public async Task HandleEventAsync(OrderPlacedEto eventData)
-        {
-            var basket = await _basketRepository.GetAsync(eventData.Buyer.BuyerId.GetValueOrDefault());
-            basket.Clear();
-            await _basketRepository.UpdateAsync(basket);
-        }
+    [UnitOfWork]
+    public async Task HandleEventAsync(OrderPlacedEto eventData)
+    {
+        var basket = await _basketRepository.GetAsync(eventData.Buyer.BuyerId.GetValueOrDefault());
+        basket.Clear();
+        await _basketRepository.UpdateAsync(basket);
     }
 }
