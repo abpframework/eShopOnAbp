11d1e54fca35f17a7641bb51680eabbd40faa681
Galip Tolga Erdem <galiperdem@gmail.com>
2023-12-26 14:14:25 -0500

    Updated BasketRepository

--- services/basket/src/EShopOnAbp.BasketService/Entities/BasketRepository.cs
+++ services/basket/src/EShopOnAbp.BasketService/Entities/BasketRepository.cs
@@ -4,13 +4,15 @@ namespace EShopOnAbp.BasketService.Entities;
 
 public class BasketRepository : IBasketRepository
 {
+    public bool? IsChangeTrackingEnabled { get; set; }
+
     private readonly IDistributedCache<Basket, Guid> _cache;
 
     public BasketRepository(IDistributedCache<Basket, Guid> cache)
     {
         _cache = cache;
     }
-    
+
     public async Task<Basket> GetAsync(Guid id)
     {
         return await _cache.GetOrAddAsync(id, () => Task.FromResult(new Basket(id)));
