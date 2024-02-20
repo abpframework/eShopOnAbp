462f68a0d192d13b3877075909e44e4d28588e8a
Galip Tolga Erdem <galiperdem@gmail.com>
2024-02-20 00:29:21 -0500

    Added footer component

---
+++ apps/public-web/src/EShopOnAbp.PublicWeb/Components/Toolbar/Footer/FooterComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace EShopOnAbp.PublicWeb.Components.Toolbar.Footer
+{
+    public class FooterComponent : AbpViewComponent
+    {
+        public virtual IViewComponentResult Invoke()
+        {
+            return View("~/Components/Toolbar/Footer/Default.cshtml");
+        }
+    }
+}
