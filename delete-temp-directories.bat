c4f78f5921e6380529f89b34ce6fec5e79291811
Sajad Afaghiy <sajadafaghiy@gmail.com>
2024-04-01 14:30:43 +0330

    Update delete-temp-directories.bat
    Refactor and user experience improvements.

--- delete-temp-directories.bat
+++ delete-temp-directories.bat
@@ -1,16 +1,21 @@
-@ECHO off
+@echo off
 cls
 
-ECHO Deleting all BIN and OBJ folders...
-ECHO.
+set directories= bin, obj, logs, node_modules
+set /a count = 0
 
-FOR /d /r . %%d in (bin,obj, logs, node_modules) DO ((
-			ECHO.Deleting: %%d
-			rd /s/q "%%d"
-		)
-	)
-)
+echo Deleting all following directories:
+for %%d in (%directories%) do echo %%d
+echo.
 
-ECHO.
-ECHO.BIN and OBJ folders have been successfully deleted. Press any key to exit.
+FOR /d /r . %%d in (%directories%) DO if exist "%%d" (
+    echo. Deleting: %%d
+    rd /s/q "%%d"
+    set /a count += 1
+    )
+
+echo.
+echo The specified directories were successfully scanned.
+echo Number of deleted directories: %count%
+echo Press any key to exit.
 pause > nul
