0ff5085fb7ae3028f92b927f382aa3539fe9a11b
Galip Tolga Erdem <galiperdem@gmail.com>
2024-05-20 16:19:29 -0400

    Updated catalog service

--- aspire/AppHost/Program.cs
+++ aspire/AppHost/Program.cs
@@ -4,8 +4,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 var administrationService =
     builder.AddProject<Projects.EShopOnAbp_AdministrationService_HttpApi_Host>("administrationService");
 var identityService = builder.AddProject<Projects.EShopOnAbp_IdentityService_HttpApi_Host>("identityService");
-var basketService = builder.AddProject<Projects.EShopOnAbp_BasketService>("basketService");
 var catalogService = builder.AddProject<Projects.EShopOnAbp_CatalogService_HttpApi_Host>("catalogService");
+var basketService = builder.AddProject<Projects.EShopOnAbp_BasketService>("basketService")
+    .WithReference(catalogService);
 var cmsKitService = builder.AddProject<Projects.EShopOnAbp_CmskitService_HttpApi_Host>("cmsKitService");
 var orderingService = builder.AddProject<Projects.EShopOnAbp_OrderingService_HttpApi_Host>("orderingService");
 var paymentService = builder.AddProject<Projects.EShopOnAbp_PaymentService_HttpApi_Host>("paymentService");
